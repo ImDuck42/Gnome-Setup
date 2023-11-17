ad2845c2087470deae7ed382116e003ac8cd4389
ImDuck42 <137700080+ImDuck42@users.noreply.github.com>
2023-11-17 19:14:07 +0100

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Check if run as root
+# Text colors
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+LIGHT_YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_MAGENTA='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
 
+# Check if run as root
 if [ "$(id -u)" -ne 0 ]; then
     echo "Please run this script as root."
     exit 1
 fi
-
 
 # Update
 sudo pacman -Syuu
@@ -19,16 +34,13 @@ sudo pacman -S appimagelauncher discord spotify spicetify-cli totem lolipop gimp
 
 
 # Enable Cups
-
 sudo systemctl enable org.cups.cupsd
 sudo systemctl start org.cups.cupsd
 
 
 # Clone all files for the Theme
-
 cd ..
 mkdir .Applications
-
 cd Gnome-Setup
 
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
@@ -36,12 +48,17 @@ git clone https://github.com/alvatip/Nordzy-icon.git
 git clone https://github.com/alvatip/Nordzy-cursors.git
 
 
-# Install the Theme, Icon and Cursor
-
+# Install the WhiteSur gtk/GDM theme and tweaks
 cd WhiteSur-gtk-theme
 ./install.sh --nord -l -c Dark -m -p 60 -P bigger --normal
+cp Dark_moon.png WhiteSur-gtk-theme
+cd WhiteSur-gtk-theme
+gsettings set org.gnome.desktop.background picture-uri 'Dark_Moon.png'
+sudo ./tweaks.sh -g -n -b 'Dark_Moon.png'
+sudo ./tweaks.sh -F -d
 cd ..
 
+# Install the Iconpack and Cursor
 cd Nordzy-icon
 ./install.sh -t default -c -p
 cd ..
@@ -51,24 +68,11 @@ cd Nordzy-cursors
 cd ..
 
 
-# Install Desktop Background and GDM Theme 
-
-cp Dark_moon.png WhiteSur-gtk-theme
-cd WhiteSur-gtk-theme
-gsettings set org.gnome.desktop.background picture-uri 'Dark_Moon.png'
-sudo ./tweaks.sh -g -n -b 'Dark_Moon.png'
-sudo ./tweaks.sh -F -d
-cd ..
-
-
 # Install Vencord 
-
 sudo sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
 killall Discord
 
-
 # Aply Spicetify and install The theme
-
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 
@@ -76,17 +80,12 @@ spicetify backup apply
 
 curl -fsSL https://raw.githubusercontent.com/Tetrax-10/Nord-Spotify/master/install-scripts/install.sh | sh
 
-
 # Delete the Gnome-Setup folder
-
 cd ..
 sudo rm -r Gnome-Setup
 
-
 # Set default shell to fish
-
 chsh -s /usr/bin/fish
-
 
 # Restart
 reboot
